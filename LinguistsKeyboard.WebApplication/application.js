b81ea2ee81a099664c038d237f71481c9bf2a96f
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2019-04-20 22:40:34 +0100

    Project setup

---
+++ LinguistsKeyboard.WebApplication/application.js
@@ -0,0 +1,20 @@
+﻿
+var application = angular.module("LinguistsKeyboard", ["ngRoute"]);
+
+
+application.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "keyboard.html", controller: "KeyboardController" })
+        .when("/settings", { templateUrl: "settings.html", controller: "SettingsController" });
+});
+
+
+
+application.controller("KeyboardController", ["$scope", function KeyboardController($scope) {
+
+    $scope.row1Keys = "1234567890-=";
+    $scope.row2Keys = "qwertyuiop[]";
+    $scope.row3Keys = "asdfghjkl;'#";
+    $scope.row4Keys = "\\zxcvbnm,./";
+
+}]);

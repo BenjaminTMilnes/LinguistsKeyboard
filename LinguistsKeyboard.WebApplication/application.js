fa03207356fece0d4014c276b6b52f32331b15c3
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-05-08 14:21:04 +0100

    Began adding German keyboard

--- LinguistsKeyboard.WebApplication/application.js
+++ LinguistsKeyboard.WebApplication/application.js
@@ -36,9 +36,9 @@ class Key {
 
 
 class Keyboard {
-    constructor(name,   lowerShift, upperShift, alternateLowerShift, alternateUpperShift) {
+    constructor(name, lowerShift, upperShift, alternateLowerShift, alternateUpperShift) {
         this.name = name;
-        this.lowerShift =   lowerShift.split("|");
+        this.lowerShift = lowerShift.split("|");
         this.upperShift = upperShift.split("|");
         this.alternateLowerShift = alternateLowerShift.split("|");
         this.alternateUpperShift = alternateUpperShift.split("|");
@@ -61,7 +61,7 @@ class Keyboard {
         }
 
         for (var i = 36; i < 47; i++) {
-            this.row4Keys.push(   this.getKey(i))
+            this.row4Keys.push(this.getKey(i))
         }
     }
 
@@ -112,14 +112,14 @@ function isAnyOneOf(characters, character) {
 }
 
 function applyDiacritic(set, diacritic) {
-   return   set.split("").map(c => (isAnyOneOf(lowercaseEnglishLetters + uppercaseEnglishLetters, c)) ? c + diacritic : c).join("|");
+    return set.split("").map(c => (isAnyOneOf(lowercaseEnglishLetters + uppercaseEnglishLetters, c)) ? c + diacritic : c).join("|");
 }
 
 
-class DiacriticModifierKeyboard  extends Keyboard{
+class DiacriticModifierKeyboard extends Keyboard {
     constructor(name, diacritic) {
         super(name, applyDiacritic(defaultKeyboardLowerShiftRegister, diacritic), applyDiacritic(defaultKeyboardUpperShiftRegister, diacritic), BLANK.split("").join("|"), BLANK.split("").join("|"));
- }
+    }
 }
 
 
@@ -131,20 +131,20 @@ const lowercaseEnglishLetters = "qwertyuiopasdfghjklzxcvbnm";
 const uppercaseEnglishLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 const defaultKeyboard = new Keyboard("English", defaultKeyboardLowerShiftRegister.split("").join("|"), defaultKeyboardUpperShiftRegister.split("").join("|"), BLANK.split("").join("|"), BLANK.split("").join("|"));
-const englishMacron =   new DiacriticModifierKeyboard("English + Macrons", "\u0304");
+const englishMacron = new DiacriticModifierKeyboard("English + Macrons", "\u0304");
 const englishAcute = new DiacriticModifierKeyboard("English + Acute Accents", "\u0301");
 const englishDiacritics = new Keyboard("English + Diacritics", defaultKeyboardLowerShiftRegister.split("").join("|"), defaultKeyboardUpperShiftRegister.split("").join("|"), "\u0301|\u0300|\u0302|\u030C|\u0306|\u0304|\u0307|\u0308|\u0303|\u030A" + BLANK.substr(10).split("").join("|"), BLANK.split("").join("|"));
 const greek = new Keyboard("Greek", "1234567890-=χωερτυ ιοπ[]ασδφθη κλ;'#\\ζξγψβνμ,./".split("").join("|"), "!\"£$%^&*()_+ΧΩΕΡΤΥ ΙΟΠ{}ΑΣΔΦΘΗ ΚΛ:@~ ΖΞΓΨΒΝΜ<>?".split("").join("|"), "\u0301\u0308                       ς                       ".split("").join("|"), BLANK.split("").join("|"));
-const mathematics1 = new Keyboard("Mathematics 1", "1234567890\u2212\u002bqwertyuiop()asdfghjkl;'#\\zxcvbnm,./".split("").join("|"), "!\"£$%^&\u00d7  _+QWERTYUIOP{}ASDFGHJKL:@~ ZXCVBNM<>?".split("").join("|"), "\u2081\u2082\u2083\u2084\u2085\u2086\u2087\u2088\u2089\u2080\u208b\u208aχωερτυ ιοπ[]ασδφθη κλ    ζξγψβνμ\u226a\u226b ".split("").join("|"), "\u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070\u207b\u207aΧΩΕΡΤΥ ΙΟΠ  ΑΣΔΦΘΗ ΚΛ    ΖΞΓΨΒΝΜ\u2264\u2265 ".split("").join("|"))
-
+const mathematics1 = new Keyboard("Mathematics 1", "1234567890\u2212\u002bqwertyuiop()asdfghjkl;'#\\zxcvbnm,./".split("").join("|"), "!\"£$%^&\u00d7  _+QWERTYUIOP{}ASDFGHJKL:@~ ZXCVBNM<>?".split("").join("|"), "\u2081\u2082\u2083\u2084\u2085\u2086\u2087\u2088\u2089\u2080\u208b\u208aχωερτυ ιοπ[]ασδφθη κλ    ζξγψβνμ\u226a\u226b ".split("").join("|"), "\u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070\u207b\u207aΧΩΕΡΤΥ ΙΟΠ  ΑΣΔΦΘΗ ΚΛ    ΖΞΓΨΒΝΜ\u2264\u2265 ".split("").join("|"));
+const T1German = new Keyboard("German (T1)", "1234567890ß qwertzuiopü+asdfghjklöä#<yxcvbnm,.-".split("").join("|"), "!\"§$%&/()=? QWERTZUIOP *ASDFGHJKL  '>YXCVBNM;:_".split("").join("|"), "                                                ".split("").join("|"), "                                                ".split("").join("|"));
 
 
 
 application.controller("KeyboardController", ["$scope", function KeyboardController($scope) {
-    
+
     $scope.currentKeyboard = defaultKeyboard;
     $scope.currentKeyboardIndex = 0;
-    $scope.availableKeyboards = [defaultKeyboard, englishDiacritics, greek, mathematics1];
+    $scope.availableKeyboards = [defaultKeyboard, englishDiacritics, greek, mathematics1, T1German];
 
     $scope.shiftIsDown = false;
     $scope.altIsDown = false;
@@ -164,7 +164,7 @@ application.controller("KeyboardController", ["$scope", function KeyboardControl
             $scope.currentKeyboardIndex = $scope.availableKeyboards.length - 1;
         }
         if ($scope.currentKeyboardIndex < 0) {
-            $scope.currentKeyboardIndex =0;
+            $scope.currentKeyboardIndex = 0;
         }
     }
 
@@ -201,12 +201,7 @@ application.controller("KeyboardController", ["$scope", function KeyboardControl
         var l = $scope.mainOutput.length;
 
         if (l > 0) {
-            if (l > 1 && $scope.mainOutput.slice(l - 1) == "\u0304") {
-                $scope.mainOutput = $scope.mainOutput.slice(0, l - 2);
-            }
-            else {
-                $scope.mainOutput = $scope.mainOutput.slice(0, l - 1);
-            }
+            $scope.mainOutput = $scope.mainOutput.slice(0, l - 1);
         }
     }
 
@@ -216,7 +211,7 @@ application.controller("KeyboardController", ["$scope", function KeyboardControl
             var i = defaultKeyboardLowerShiftRegister.indexOf(event.key);
 
             if (i < 0) {
-                 i = defaultKeyboardUpperShiftRegister.indexOf(event.key);
+                i = defaultKeyboardUpperShiftRegister.indexOf(event.key);
             }
 
             if (i >= 0) {
@@ -328,5 +323,12 @@ application.controller("KeyboardController", ["$scope", function KeyboardControl
         }
 
     }
+
+}]);
+
+
+application.controller("SettingsController", ["$scope", function SettingsController($scope) {
+
+
 
 }]);

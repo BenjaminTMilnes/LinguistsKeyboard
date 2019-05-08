c11c8f3d753f184a5bda4ceac2988e484a3937e5
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-05-08 13:50:34 +0100

    Added more to maths keyboard

--- LinguistsKeyboard.WebApplication/application.js
+++ LinguistsKeyboard.WebApplication/application.js
@@ -135,7 +135,7 @@ const englishMacron =   new DiacriticModifierKeyboard("English + Macrons", "\u03
 const englishAcute = new DiacriticModifierKeyboard("English + Acute Accents", "\u0301");
 const englishDiacritics = new Keyboard("English + Diacritics", defaultKeyboardLowerShiftRegister.split("").join("|"), defaultKeyboardUpperShiftRegister.split("").join("|"), "\u0301|\u0300|\u0302|\u030C|\u0306|\u0304|\u0307|\u0308|\u0303|\u030A" + BLANK.substr(10).split("").join("|"), BLANK.split("").join("|"));
 const greek = new Keyboard("Greek", "1234567890-=χωερτυ ιοπ[]ασδφθη κλ;'#\\ζξγψβνμ,./".split("").join("|"), "!\"£$%^&*()_+ΧΩΕΡΤΥ ΙΟΠ{}ΑΣΔΦΘΗ ΚΛ:@~ ΖΞΓΨΒΝΜ<>?".split("").join("|"), "\u0301\u0308                       ς                       ".split("").join("|"), BLANK.split("").join("|"));
-const mathematics1 = new Keyboard("Mathematics 1", "1234567890\u2212\u002bqwertyuiop()asdfghjkl;'#\\zxcvbnm,./".split("").join("|"), "!\"£$%^&\u00d7  _+QWERTYUIOP{}ASDFGHJKL:@~ ZXCVBNM<>?".split("").join("|"), "            χωερτυ ιοπ[]ασδφθη κλ    ζξγψβνμ   ".split("").join("|"), "\u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070  ΧΩΕΡΤΥ ΙΟΠ  ΑΣΔΦΘΗ ΚΛ    ΖΞΓΨΒΝΜ   ".split("").join("|"))
+const mathematics1 = new Keyboard("Mathematics 1", "1234567890\u2212\u002bqwertyuiop()asdfghjkl;'#\\zxcvbnm,./".split("").join("|"), "!\"£$%^&\u00d7  _+QWERTYUIOP{}ASDFGHJKL:@~ ZXCVBNM<>?".split("").join("|"), "\u2081\u2082\u2083\u2084\u2085\u2086\u2087\u2088\u2089\u2080\u208b\u208aχωερτυ ιοπ[]ασδφθη κλ    ζξγψβνμ\u226a\u226b ".split("").join("|"), "\u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070\u207b\u207aΧΩΕΡΤΥ ΙΟΠ  ΑΣΔΦΘΗ ΚΛ    ΖΞΓΨΒΝΜ\u2264\u2265 ".split("").join("|"))
 
 
 
@@ -212,7 +212,7 @@ application.controller("KeyboardController", ["$scope", function KeyboardControl
 
     $scope.keyDown = function (event) {
 
-        if (!$scope.controlIsDown) {
+        if (!$scope.controlIsDown && event.code != "Space") {
             var i = defaultKeyboardLowerShiftRegister.indexOf(event.key);
 
             if (i < 0) {

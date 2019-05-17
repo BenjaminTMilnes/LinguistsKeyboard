31f8b5ea0eec803790a80bdb217f7b419b0ca806
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-05-17 23:27:31 +0100

    Made Old English keyboard file

---
+++ LinguistsKeyboard.WebApplication/core.js
@@ -0,0 +1,119 @@
+﻿
+function isAnyOneOf(characters, character) {
+    return (characters.split("").filter(c => c == character).length > 0);
+}
+
+class Key {
+    constructor(lowerShift, upperShift, alternateLowerShift, alternateUpperShift) {
+        this.lowerShift = lowerShift;
+        this.upperShift = upperShift;
+        this.alternateLowerShift = alternateLowerShift;
+        this.alternateUpperShift = alternateUpperShift;
+    }
+
+    get l() {
+        return this.lowerShift;
+    }
+
+    get u() {
+        return this.upperShift;
+    }
+
+    get al() {
+        return this.alternateLowerShift;
+    }
+
+    get au() {
+        return this.alternateUpperShift;
+    }
+}
+
+class Keyboard {
+    constructor(name, abbreviatedName, reference, isSelected, lowerShift, upperShift, alternateLowerShift, alternateUpperShift) {
+        this.name = name;
+        this.abbreviatedName = abbreviatedName;
+        this.reference = reference;
+        this.isSelected = isSelected;
+
+        this.lowerShift = lowerShift.split("|");
+        this.upperShift = upperShift.split("|");
+        this.alternateLowerShift = alternateLowerShift.split("|");
+        this.alternateUpperShift = alternateUpperShift.split("|");
+
+        this.row1Keys = [];
+        this.row2Keys = [];
+        this.row3Keys = [];
+        this.row4Keys = [];
+
+        for (var i = 0; i < 12; i++) {
+            this.row1Keys.push(this.getKey(i))
+        }
+
+        for (var i = 12; i < 24; i++) {
+            this.row2Keys.push(this.getKey(i))
+        }
+
+        for (var i = 24; i < 36; i++) {
+            this.row3Keys.push(this.getKey(i))
+        }
+
+        for (var i = 36; i < 47; i++) {
+            this.row4Keys.push(this.getKey(i))
+        }
+    }
+
+    get l() {
+        return this.lowerShift;
+    }
+
+    get u() {
+        return this.upperShift;
+    }
+
+    get al() {
+        return this.alternateLowerShift;
+    }
+
+    get au() {
+        return this.alternateUpperShift;
+    }
+
+    getKey(i) {    
+            return new Key(this.l[i], this.u[i], this.al[i], this.au[i]);     
+    }
+
+    getLetter(forDefault) {
+        var i = -1;
+
+        i = defaultKeyboardLowerShiftRegister.indexOf(forDefault);
+
+        if (i >= 0) {
+            return this.getKey(i);
+        }
+
+        i = defaultKeyboardUpperShiftRegister.indexOf(forDefault);
+
+        if (i >= 0) {
+            return this.getKey(i);
+        }
+    }
+}
+
+function applyDiacritic(set, diacritic) {
+    return set.split("").map(c => (isAnyOneOf(lowercaseEnglishLetters + uppercaseEnglishLetters, c)) ? c + diacritic : c).join("|");
+}
+
+class DiacriticModifierKeyboard extends Keyboard {
+    constructor(name, diacritic) {
+        super(name, applyDiacritic(defaultKeyboardLowerShiftRegister, diacritic), applyDiacritic(defaultKeyboardUpperShiftRegister, diacritic), BLANK.split("").join("|"), BLANK.split("").join("|"));
+    }
+}
+
+const defaultKeyboardLowerShiftRegister = "1234567890-=qwertyuiop[]asdfghjkl;'#\\zxcvbnm,./";
+const defaultKeyboardUpperShiftRegister = "!\"£$%^&*()_+QWERTYUIOP{}ASDFGHJKL:@~ ZXCVBNM<>?";
+const BLANK = "                                                ";
+
+const lowercaseEnglishLetters = "qwertyuiopasdfghjklzxcvbnm";
+const uppercaseEnglishLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+const defaultKeyboard = new Keyboard("English (UK)", "English", "english1", true, defaultKeyboardLowerShiftRegister.split("").join("|"), defaultKeyboardUpperShiftRegister.split("").join("|"), BLANK.split("").join("|"), BLANK.split("").join("|"));
